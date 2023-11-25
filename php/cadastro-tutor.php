6597baee8597e731756de47f8bac923e64449d27
Alan Gonçalves <alangnclvs@outlook.com>
2023-11-25 01:58:44 -0300

    update

--- php/cadastro-tutor.php
+++ php/cadastro-tutor.php
@@ -130,17 +130,6 @@ if (isset($_GET['idTutor'])) {
         </div>
     </div>
 
-    <!-- Modal Login -->
-    <div id="modalContainer"></div>
-    <script>
-        // Use JavaScript para carregar o conteúdo do modal-login.html no elemento modalContainer
-        fetch('modal-login.html')
-            .then(response => response.text())
-            .then(data => {
-                document.getElementById('modalContainer').innerHTML = data;
-            });
-    </script>
-
     <!-- Import JavaScript bootstrap -->
     <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
